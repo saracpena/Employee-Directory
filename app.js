5b74de535947368ae90d54157f607b867ffefa36
Sara <s.cristal.p@gmail.com>
2026-07-15 12:46:56 +0200

    add comments

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+
+/* Boiler Plate
+ app.get(
+    route,
+    (req, res) => {
+        res.send(...)
+    }
+);*/
 app.get("/", (req,res) => {
     res.send("Hello, Employees!");
 });
